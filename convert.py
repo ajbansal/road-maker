3acfc74dbc7d463599c124f4b3f35eeab688af3c
abhijit.bansal <abhijit.bansal@ff.com>
2017-09-18 23:17:33 -0700

    - Added some convert functions - Get points between two points

---
+++ convert.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env/python
+"""
+Title:
+    Python script for 
+
+Assumption:
+    1. All the python packages are installed
+    2. You are using python==2.7.11
+    3. Any more assumptions <>
+
+Description:
+    1. Describe the class
+
+Author:
+    Abhijit Bansal
+
+Copyright:
+    (C) 2016 Faraday Future
+
+"""
+from math import radians, cos, sin, asin, sqrt, atan2, degrees
+
+
+def calculate_distance(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    credit - Michael Dunn
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
+
+def calculate_bearing(long1, lat1, long2, lat2):
+    """Returns bearing in degrees"""
+    bearing = atan2(sin(long2 - long1) * cos(lat2), cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(long2 - long1))
+    bearing = degrees(bearing)
+    bearing = (bearing + 360) % 360
+    return bearing
+
+
+def calc_centre_lane(points):
+    centre_lane = [(0, 0)]
+    for idx, point in enumerate(points[1:], 1):
+        curr_long, curr_lat = point
+        prev_long, prev_lat = points[idx - 1]
+
+        dist = calculate_distance(prev_long, prev_lat, curr_long, curr_lat)
+        bearing = calculate_bearing(prev_long, prev_lat, curr_long, curr_lat)
+
+        prev_x, prev_y = centre_lane[idx - 1]
+
+        curr_x = prev_x + (cos(radians(bearing)) * dist)
+        curr_y = prev_y + (sin(radians(bearing)) * dist)
+
+        centre_lane.append((curr_x, curr_y))
+    return centre_lane
--- road_maker.py
+++ road_maker.py
@@ -18,3 +18,33 @@ Copyright:
     (C) 2016 Faraday Future
 
 """
+import googlemaps
+from datetime import datetime
+
+import math
+
+from convert import calc_centre_lane
+import polyline
+
+gmaps = googlemaps.Client(key='AIzaSyCHDHUGzAhKsViuASECNblUNA69545Hr5Y')
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("3165 Sawtelle Blvd, Los Angeles, CA 90066",
+                                     "18455 S Figueroa St, Gardena, CA 90248",
+                                     mode="driving",
+                                     departure_time=now)
+
+set_of_long_lat_1 = polyline.decode(directions_result[0]['overview_polyline']['points'])
+cl_1 = calc_centre_lane(set_of_long_lat_1)
+
+directions_result = gmaps.directions((34.024552, -118.445100), (34.028243, -118.442965), mode='walking')
+set_of_long_lat_2 = polyline.decode(directions_result[0]['overview_polyline']['points'])
+cl_2 = calc_centre_lane(set_of_long_lat_2)
+
