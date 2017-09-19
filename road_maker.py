a1fa1669365d41c7cd095d3a9a9008f02037d9f3
abhijit.bansal <abhijit.bansal@ff.com>
2017-09-18 22:03:09 -0700

    Initial commit

---
+++ road_maker.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env/python
+"""
+Title:
+    Python script for 
+
+Assumption:
+    1. All the python packages are installed
+    2. You are using python==2.7.11
+    3. Any more assumptions <>
+
+Description:
+    1. Describe the class
+
+Author:
+    Abhijit Bansal
+
+Copyright:
+    (C) 2016 Faraday Future
+
+"""
